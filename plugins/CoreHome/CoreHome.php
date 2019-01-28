87fc16923bd9edfdf20899f5a24d8f451738e3f3
sgiehl <stefan@matomo.org>
2019-01-28 12:37:25 +0100

    Use datatable config to allow splitting dimensions into separate columns

--- plugins/CoreHome/CoreHome.php
+++ plugins/CoreHome/CoreHome.php
@@ -326,6 +326,8 @@ class CoreHome extends \Piwik\Plugin
         $translationKeys[] = 'CoreHome_ExcludeRowsWithLowPopulation';
         $translationKeys[] = 'CoreHome_DataTableIncludeAggregateRows';
         $translationKeys[] = 'CoreHome_DataTableExcludeAggregateRows';
+        $translationKeys[] = 'CoreHome_DataTableCombineDimensions';
+        $translationKeys[] = 'CoreHome_DataTableShowDimensions';
         $translationKeys[] = 'CoreHome_Default';
         $translationKeys[] = 'CoreHome_PageOf';
         $translationKeys[] = 'CoreHome_FormatMetrics';
